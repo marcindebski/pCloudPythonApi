ad904bc1122c6f68529394a9d20f8e993ae2dc6c
Marcin Debski <debski.marcin.r@gmail.com>
2017-12-21 22:27:24 +0100

    Added files for Travis CI

---
+++ tests/test_api.py
@@ -0,0 +1,18 @@
+""" Unittests of pCloud API """
+import unittest
+
+
+class TestpCloudApi(unittest.TestCase):
+    """ Test pCloud API """
+
+    def test_login(self):
+        """ Test login method """
+        pass
+
+    def test_logout(self):
+        """ Test logout method """
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
