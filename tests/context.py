c3c4dcaeff1ba2d620b279922d67845700fd5b9b
Marcin Debski <debski.marcin.r@gmail.com>
2017-12-22 22:43:07 +0100

    Added first test

---
+++ tests/context.py
@@ -0,0 +1,7 @@
+""" Dependencies for tests """
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
+from pcloud_api import pCloudApi
+from pcloud_api import AuthenticationError
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,17 +1,18 @@
-""" Unittests of pCloud API """
+""" Tests of pCloud API """
 import unittest
+from context import pCloudApi
+from context import AuthenticationError
 
 
-class TestpCloudApi(unittest.TestCase):
-    """ Test pCloud API """
+class TestPCloudApi(unittest.TestCase):
+    """ Tests of pCloud API """
 
-    def test_login(self):
-        """ Test login method """
-        pass
-
-    def test_logout(self):
-        """ Test logout method """
-        pass
+    def test_invalid_username_password(self):
+        """ Test if cannot log in using invalid credentials """
+        pcloud_invalid_client = pCloudApi('itdoesnotexistforsure@blah.pl', 'password')
+        with self.assertRaises(AuthenticationError) as ctx:
+            pcloud_invalid_client.login()
+        self.assertTrue('Cannot authenticate' in str(ctx.exception))
 
 
 if __name__ == '__main__':
